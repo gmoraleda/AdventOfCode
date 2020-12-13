28d5466214276dab0a16a69effed575ce4049b65
Guillermo Moraleda <guillermo.moraleda@cluno.com>
2020-12-13 20:30:51 +0100

    part 2

--- 2020/day9.dart
+++ 2020/day9.dart
@@ -20,28 +20,25 @@ main() {
       }
     }
     if (!valid) {
-      print('Invalid: $number');
-      exit(1);
+      print('Part I: $number');
+      break;
     }
   }
 
   // Part 2
-  windowSize = 3;
   var target = 1721308972;
-  for (var i = windowSize; i < input.length; i++) {
-    var number = input[i];
-    var window = input.sublist(i - windowSize, i);
+  var indexOfTarget = input.indexOf(target);
 
-    var valid = false;
-    for (var element in window) {
-      if (window.contains(number - element)) {
-        valid = true;
-        break;
+  for (var i = indexOfTarget - 1; i >= 0; i--) {
+    for (var j = i - 1; j >= 0; j--) {
+      var window = input.sublist(j, i);
+      var sum = window.reduce((value, element) => value + element);
+
+      if (sum == target) {
+        window.sort();
+        print('Part II: ${window.first + window.last}');
+        exit(1);
       }
-    }
-    if (!valid) {
-      print('Invalid: $number');
-      exit(1);
     }
   }
 }
