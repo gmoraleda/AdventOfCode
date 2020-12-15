dd2e382def6a42742128f2bc76ddfb873ff8c0ec
Guillermo Moraleda <guillermo.moraleda@cluno.com>
2020-12-15 20:24:59 +0100

    Part 1

--- 2020/day11.dart
+++ 2020/day11.dart
@@ -6,8 +6,97 @@ main() {
   final file = new File(path);
   final input = file.readAsLinesSync().map((e) => e.split('')).toList();
 
-  printArray(input);
-  print(countSeats(input));
+  var output = update(input);
+
+  while (true) {
+    var temp = update(output);
+    output = temp;
+    print('${countSeats(output)}');
+  }
+}
+
+List<List<String>> update(List<List<String>> input) {
+  final List<List<String>> copy =
+      input.map((element) => List<String>.from(element)).toList();
+
+  for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < input.first.length; j++) {
+      switch (input[i][j]) {
+        case '.':
+          copy[i][j] = '.';
+          continue;
+        case 'L':
+          copy[i][j] = sit(input, i, j, "L");
+          continue;
+        case '#':
+          copy[i][j] = sit(input, i, j, "#");
+          continue;
+      }
+    }
+  }
+  return copy;
+}
+
+String sit(List<List<String>> input, int i, int j, String mode) {
+  var count = 0;
+  var sit = true;
+
+  // Right
+  if (input.tryGet(i, j + 1) != null && input[i][j + 1] == '#') {
+    count++;
+    sit = false;
+  }
+  // Left
+  if (input.tryGet(i, j - 1) != null && input[i][j - 1] == '#') {
+    count++;
+    sit = false;
+  }
+  // Up
+  if (input.tryGet(i - 1, j) != null && input[i - 1][j] == '#') {
+    count++;
+    sit = false;
+  }
+  // Down
+  if (input.tryGet(i + 1, j) != null && input[i + 1][j] == '#') {
+    count++;
+    sit = false;
+  }
+  // Up-Right
+  if (input.tryGet(i - 1, j + 1) != null && input[i - 1][j + 1] == '#') {
+    count++;
+    sit = false;
+  }
+  // Up-Left
+  if (input.tryGet(i - 1, j - 1) != null && input[i - 1][j - 1] == '#') {
+    count++;
+    sit = false;
+  }
+  // Down-Right
+  if (input.tryGet(i + 1, j + 1) != null && input[i + 1][j + 1] == '#') {
+    count++;
+    sit = false;
+  }
+  // Down-Left
+  if (input.tryGet(i + 1, j - 1) != null && input[i + 1][j - 1] == '#') {
+    count++;
+    sit = false;
+  }
+
+  if (mode == "#") {
+    if (count >= 4) {
+      return 'L';
+    } else if (input[i][j] == '.') {
+      return '.';
+    }
+    return '#';
+  } else {
+    if (sit) {
+      return '#';
+    } else if (input[i][j] == '.') {
+      return '.';
+    }
+    return 'L';
+  }
 }
 
 void printArray(List<List<String>> input) {
@@ -23,7 +112,14 @@ void printArray(List<List<String>> input) {
 int countSeats(List<List<String>> input) {
   return input.fold(0, (int value, element) {
     final rowCount = element.fold(
-        0, (int value, element) => value += element == 'L' ? 1 : 0);
+        0, (int value, element) => value += element == '#' ? 1 : 0);
     return value + rowCount;
   });
+}
+
+extension ListGetExtension<T> on List<List<T>> {
+  T tryGet(int i, int j) =>
+      i < 0 || j < 0 || i >= this.length || j >= this.first.length
+          ? null
+          : this[i][j];
 }
