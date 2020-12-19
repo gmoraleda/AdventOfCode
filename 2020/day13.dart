01ab625b48c40b9e8a88b459e1e9331d2cfa25a3
Guillermo Moraleda <guillermo.moraleda@cluno.com>
2020-12-19 13:14:12 +0100

    part2

--- 2020/day13.dart
+++ 2020/day13.dart
@@ -19,4 +19,24 @@ main() {
   final busID = buses[departures.indexOf(firstDeparture)];
 
   print((firstDeparture - timestamp) * busID);
+
+  // Part II (from: https://www.youtube.com/watch?v=4_5mluiXF5I)
+  final departureList = input.last.split(',').toList();
+  final busList = departureList.map((e) {
+    if (int.tryParse(e) == null) {
+      return 1;
+    }
+    return int.parse(e);
+  }).toList();
+
+  var t = 0;
+  var stepSize = busList[0];
+  for (var i = 1; i < busList.length; i++) {
+    final bus = busList[i];
+    while (((t + i) % bus != 0)) {
+      t += stepSize;
+    }
+    stepSize *= bus;
+  }
+  print(t);
 }
