ea30bb1eab62846c12cb6146664084cdbd2c2f99
Guillermo Moraleda <guillermo.moraleda@cluno.com>
2020-12-21 14:21:12 +0100

    part2

--- 2020/day16.dart
+++ 2020/day16.dart
@@ -6,7 +6,8 @@ main() {
   final file = new File(path);
   final input = file.readAsStringSync().split('\n\n');
   final rangesRaw = input[0];
-  final myTickets = input[1];
+  final myTicket =
+      input[1].split('\n').last.split(',').map((e) => int.parse(e)).toList();
   var nearbyTickets = List<int>();
   input[2].split('\n').sublist(1).forEach((row) {
     nearbyTickets.addAll(row.split(',').map((e) => int.parse(e)));
@@ -39,11 +40,76 @@ main() {
 
   // Part I
   print(invalidTickets.reduce((value, element) => value + element));
+
+  // Part II
+  final departureRanges = ranges;
+  var rangePairs = [];
+  for (var i = 0; i < departureRanges.length; i += 2) {
+    rangePairs.add(RangePair(a: departureRanges[i], b: departureRanges[i + 1]));
+  }
+
+  final filteredNearbyTickets = input[2].split('\n').sublist(1).map((row) {
+    return row.split(',').map((e) => int.parse(e)).toList();
+  }).where((element) {
+    final firstSet = element.toSet();
+    final secondSet = invalidTickets.toSet();
+    return firstSet.intersection(secondSet).isEmpty;
+  }).toList();
+
+  var departureColumns = List<List<int>>();
+  for (var i = 0; i < rangePairs.length; i++) {
+    var currentPair = rangePairs[i];
+    var currentMatches = List<int>();
+    for (var j = 0; j < filteredNearbyTickets.first.length; j++) {
+      var column = filteredNearbyTickets.map((e) => e[j]).toList();
+      if (column.every((element) => element.inRangePair(currentPair))) {
+        currentMatches.add(j);
+      }
+    }
+    departureColumns.add(currentMatches);
+  }
+
+  var identifiedFields = List<dynamic>.generate(20, (index) => null);
+  while (identifiedFields.any((element) => element == null)) {
+    for (var column in departureColumns) {
+      if (column.length == 1) {
+        final index = departureColumns.indexOf(column);
+        final number = column.first;
+        identifiedFields[index] = number;
+
+        for (var column in departureColumns) {
+          column.remove(number);
+        }
+      }
+    }
+  }
+  var result = 1;
+  for (var i = 0; i < 6; i++) {
+    result *= myTicket[identifiedFields[i]];
+  }
+  print(result);
 }
 
 extension NumExtension on num {
   bool inRange(Range range) {
     return this >= range.min && this <= range.max;
+  }
+
+  bool inRangePair(RangePair pair) {
+    return (this.inRange(pair.a) || this.inRange(pair.b));
+  }
+}
+
+class RangePair {
+  Range a;
+  Range b;
+  RangePair({
+    this.a,
+    this.b,
+  });
+  @override
+  String toString() {
+    return "$a or $b";
   }
 }
 
