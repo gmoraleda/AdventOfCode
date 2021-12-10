c9e875e4afbd2272f3f56f86e3b1b7efdc0271f5
Guillermo Moraleda <guillermo.moraleda@capmo.de>
2021-12-10 16:16:32 +0100

    Day 9 part 2

--- 2021/09/9.rb
+++ 2021/09/9.rb
@@ -1,17 +1,21 @@
 require 'pp'
-file = File.open('09/test')
+file = File.open('09/input')
 board =
   file_data =
     file.readlines.map(&:strip).map(&:chars).map { |row| row.map(&:to_i) }
 
 class Point
   attr_accessor :value
+  attr_accessor :x
+  attr_accessor :y
   attr_accessor :top
   attr_accessor :left
   attr_accessor :right
   attr_accessor :bottom
-  def initialize(value, top, left, right, bottom)
+  def initialize(value, x, y, top, left, right, bottom)
     @value = value
+    @x = x
+    @y = y
     @top = top
     @left = left
     @right = right
@@ -38,6 +42,8 @@ for i in 0..(board.length - 1)
     points <<
       Point.new(
         get_value(board, j, i),
+        j,
+        i,
         get_value(board, j, i - 1),
         get_value(board, j - 1, i),
         get_value(board, j + 1, i),
@@ -46,38 +52,48 @@ for i in 0..(board.length - 1)
   end
 end
 
-puts 'Part 1: ' +
-       points
-         .filter { |point| point.is_low_location }
-         .map { |point| point.value + 1 }
-         .sum
-         .to_s
+lower_points = points.filter { |point| point.is_low_location }
+
+puts 'Part 1: ' + lower_points.map { |point| point.value + 1 }.sum.to_s
 
 # Part 2
 
-def check(board, x, y, sum)
-  return sum if board[y][x] == 9
+basins = {}
 
-  max_y = board.length - 1
-  max_x = board.first.length - 1
+visited = board.clone
 
-  return sum if (x < 0 || y < 0 || x >= max_x || y >= max_y)
-
-  sum += 1
-
-  return(
-    check(board, x + 1, y, sum) + check(board, x - 1, y, sum) +
-      check(board, x, y + 1, sum) + check(board, x, y - 1, sum)
-  )
+def check(board, visited, x, y, marker)
+  return if x < 0 || y < 0 || x >= board.first.size || y >= board.size
+  return if board[y][x] == 9
+  return if visited[y][x] == marker
+  visited[y][x] = marker
+  check(board, visited, x + 1, y, marker)
+  check(board, visited, x - 1, y, marker)
+  check(board, visited, x, y + 1, marker)
+  check(board, visited, x, y - 1, marker)
 end
 
-basins = Array.new
+lower_points.each_with_index do |point, index|
+  visited[point.y][point.x] = -index
 
-for i in 0..(board.length - 1)
-  for j in 0..(board[i].length - 1)
-    basin = check(board, j, i, 0)
-    basins << basin
-  end
+  check(board, visited, point.x + 1, point.y, -index)
+  check(board, visited, point.x - 1, point.y, -index)
+  check(board, visited, point.x, point.y + 1, -index)
+  check(board, visited, point.x, point.y - 1, -index)
 end
 
-pp basins
+hash = {}
+visited.each do |row|
+  row
+    .group_by(&:itself)
+    .map do |k, v|
+      if hash.has_key?(k)
+        hash[k] += v.size
+      else
+        hash[k] = v.size
+      end
+    end
+end
+
+hash.delete(9)
+puts "Part 2: #{hash.values.sort.reverse.slice(0, 3).reduce(:*)}"
