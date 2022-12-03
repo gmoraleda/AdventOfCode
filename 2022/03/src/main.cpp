258438cab014ca6ce7cb7c5ac11485f4a3ac2872
Guillermo Moraleda <guillermo.moraleda@capmo.de>
2022-12-03 13:12:32 +0100

    Day 3 P2

--- 2022/03/src/main.cpp
+++ 2022/03/src/main.cpp
@@ -39,7 +39,53 @@ int main(int argc, char **argv)
             sum += value;
         }
         std::cout << sum << std::endl;
-
         file.close();
+
+        sum = 0;
+
+        std::ifstream file(filename);
+        if (file.is_open())
+        {
+
+            // Part II
+
+            std::string elf1;
+
+            while (std::getline(file, elf1))
+            {
+                std::string elf2;
+                std::string elf3;
+
+                std::getline(file, elf2);
+                std::getline(file, elf3);
+
+                std::sort(elf1.begin(), elf1.end());
+                std::sort(elf2.begin(), elf2.end());
+                std::sort(elf3.begin(), elf3.end());
+
+                std::string string_intersection1;
+                std::string string_intersection2;
+                std::string string_intersection3;
+
+                // Intersection elf1 with elf2
+                std::set_intersection(elf1.begin(), elf1.end(), elf2.begin(), elf2.end(), std::back_inserter(string_intersection1));
+                std::set_intersection(elf1.begin(), elf1.end(), elf3.begin(), elf3.end(), std::back_inserter(string_intersection2));
+
+                // Intersection intersection1 with intersection2
+                std::set_intersection(string_intersection1.begin(), string_intersection1.end(), string_intersection2.begin(), string_intersection2.end(), std::back_inserter(string_intersection3));
+
+                int value = string_intersection3[0] - 96;
+                if (value < 0)
+                {
+                    value = string_intersection3[0] - 38;
+                }
+
+                sum += value;
+            }
+
+            std::cout << sum << std::endl;
+
+            file.close();
+        }
     }
 }
